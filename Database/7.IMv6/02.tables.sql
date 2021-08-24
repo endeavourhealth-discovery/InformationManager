d7a99fcd18be9c138e985d70f12383ae3ef968ce
Zeynep Bayazitoglu <zeynep.bayazitoglu@ergosoft.co.uk>
2021-08-24 16:16:54 +0100

    added name column in the task table

--- Database/7.IMv6/02.tables.sql
+++ Database/7.IMv6/02.tables.sql
@@ -218,6 +218,7 @@ CREATE TABLE IF NOT EXISTS task (
     dbid INT NOT NULL AUTO_INCREMENT,
     workflow INT NOT NULL,
     id VARCHAR(200) NOT NULL,
+    name VARCHAR(100) NOT NULL,
     state VARCHAR(100) NOT NULL,
 
     PRIMARY KEY task_pk (dbid),
