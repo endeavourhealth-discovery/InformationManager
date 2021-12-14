bf258b4ad4ce3c1a70144d0505212ccce5bc1014
Richard Collier <gitkraken@tirinoarim.co.uk>
2021-12-14 11:01:04 +0000

    Various fixes

--- DocumentImport/src/main/java/org/endeavourhealth/informationmanager/rdf4j/TTEntityFilerRdf4j.java
+++ DocumentImport/src/main/java/org/endeavourhealth/informationmanager/rdf4j/TTEntityFilerRdf4j.java
@@ -159,26 +159,23 @@ public class TTEntityFilerRdf4j implements TTEntityFiler {
         }
     }
 
-    private void addTriple(ModelBuilder builder, Resource subject, IRI predicate, TTArray values) throws TTFilerException {
-        for (TTValue value:values.getElements()){
-            if (value.isLiteral()) {
-                builder.add(subject, predicate, value.asLiteral().getType() == null
+    private void addTriple(ModelBuilder builder, Resource subject, IRI predicate, TTValue value) throws TTFilerException {
+
+        if (value.isLiteral()) {
+            builder.add(subject, predicate, value.asLiteral().getType() == null
                 ? literal(value.asLiteral().getValue())
                 : literal(value.asLiteral().getValue(), toIri(value.asLiteral().getType().getIri())));
-            }
-            else if (value.isIriRef()) {
-                builder.add(subject, predicate, toIri(value.asIriRef().getIri()));
-            }
-            else if (value.isNode()) {
-                TTNode node = value.asNode();
-                BNode bNode = bnode();
-                builder.add(subject, predicate, bNode);
+        } else if (value.isIriRef()) {
+            builder.add(subject, predicate, toIri(value.asIriRef().getIri()));
+        } else if (value.isNode()) {
+            TTNode node = value.asNode();
+            BNode bNode = bnode();
+            builder.add(subject, predicate, bNode);
             for (Map.Entry<TTIriRef, TTArray> entry : node.getPredicateMap().entrySet()) {
-                    addTriple(builder, bNode, toIri(entry.getKey().getIri()), entry.getValue());
-                }
-        } else {
-                throw new TTFilerException("Arrays of arrays not allowed ");
+                addTriple(builder, bNode, toIri(entry.getKey().getIri()), entry.getValue());
             }
+        } else {
+            throw new TTFilerException("Arrays of arrays not allowed ");
         }
     }
 
