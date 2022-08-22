550ebed267d40046d73387d79e71946411a3a967
Richard Collier <gitkraken@tirinoarim.co.uk>
2022-08-22 16:42:02 +0100

    Additional debug

--- Transforms/src/main/java/org/endeavourhealth/informationmanager/transforms/preload/Preload.java
+++ Transforms/src/main/java/org/endeavourhealth/informationmanager/transforms/preload/Preload.java
@@ -22,123 +22,127 @@ import java.util.Scanner;
 public class Preload {
 
 
-	public static void main(String[] args) throws Exception {
-		if (args.length < 5) {
-			System.err.println("Insufficient parameters supplied:");
-			System.err.println("source={sourcefolder} preload={foldercontaing preload} "+
-				"temp= {folder for temporary data} and privacy= {0 public, 1 private publisher 2 private for authoring} cmd={graphdbExecutable}");
-			System.exit(-1);
-		}
-		TTFilerFactory.setBulk(true);
-		String graphdbCommand=null;
+    public static void main(String[] args) throws Exception {
+        System.out.println("Running Preload...");
+        if (args.length < 5) {
+            System.err.println("Insufficient parameters supplied:");
+            System.err.println("source={sourcefolder} preload={foldercontaing preload} " +
+                "temp= {folder for temporary data} and privacy= {0 public, 1 private publisher 2 private for authoring} cmd={graphdbExecutable}");
+            System.exit(-1);
+        }
+        TTFilerFactory.setBulk(true);
+        String graphdbCommand = null;
 
-		TTImportConfig cfg = new TTImportConfig();
+        System.out.println("Configuring...");
+        TTImportConfig cfg = new TTImportConfig();
 
-			for (int i = 0; i < args.length; i++) {
-				String arg=args[i].toLowerCase();
-				if (arg.startsWith("source=")) {
-					cfg.setFolder(arg.split("=")[1]);
-					TTBulkFiler.setConfigTTl(arg.split("=")[1]);
-				}
-				else if (arg.startsWith("preload"))
-					TTBulkFiler.setPreload(arg.split("=")[1]);
-				else if (arg.startsWith("temp="))
-					TTBulkFiler.setDataPath(arg.split("=")[1]);
-				else if (arg.startsWith("privacy"))
-					TTBulkFiler.setPrivacyLevel(Integer.parseInt(arg.split("=")[1]));
-				else if (arg.startsWith("cmd"))
-					graphdbCommand= arg.split("=")[1];
-				else 	if (args[i].contains("test="))
-					ImportApp.testDirectory= args[i].substring(args[i].lastIndexOf("=")+1);
-				else
-							System.err.println("Unknown parameter " + args[i]);
-			}
-			if (graphdbCommand==null){
-				System.err.println("Must set cmd={graphexecutable} argument to start graph db at the end of the process before processing deltas");
-				System.exit(-1);
-			}
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i].toLowerCase();
+            if (arg.startsWith("source=")) {
+                cfg.setFolder(arg.split("=")[1]);
+                TTBulkFiler.setConfigTTl(arg.split("=")[1]);
+            } else if (arg.startsWith("preload"))
+                TTBulkFiler.setPreload(arg.split("=")[1]);
+            else if (arg.startsWith("temp="))
+                TTBulkFiler.setDataPath(arg.split("=")[1]);
+            else if (arg.startsWith("privacy"))
+                TTBulkFiler.setPrivacyLevel(Integer.parseInt(arg.split("=")[1]));
+            else if (arg.startsWith("cmd"))
+                graphdbCommand = arg.split("=")[1];
+            else if (args[i].contains("test="))
+                ImportApp.testDirectory = args[i].substring(args[i].lastIndexOf("=") + 1);
+            else
+                System.err.println("Unknown parameter " + args[i]);
+        }
+        if (graphdbCommand == null) {
+            System.err.println("Must set cmd={graphexecutable} argument to start graph db at the end of the process before processing deltas");
+            System.exit(-1);
+        }
 
-		importData(cfg,graphdbCommand);
-	}
+        System.out.println("Starting import...");
+        importData(cfg, graphdbCommand);
+    }
 
-	private static void importData(TTImportConfig cfg,String graphdb) throws Exception {
+    private static void importData(TTImportConfig cfg, String graphdb) throws Exception {
+        System.out.println("Validating config...");
+        validateGraphConfig(cfg.getFolder());
 
-		    validateGraphConfig(cfg.getFolder());
-				TTImportByType importer = new Importer()
-					.validateByType(IM.GRAPH_DISCOVERY, cfg.getFolder())
-					.validateByType(SNOMED.GRAPH_SNOMED, cfg.getFolder())
-					.validateByType(IM.GRAPH_ENCOUNTERS, cfg.getFolder())
-					.validateByType(IM.GRAPH_EMIS, cfg.getFolder())
-					.validateByType(IM.GRAPH_TPP, cfg.getFolder())
-					.validateByType(IM.GRAPH_OPCS4, cfg.getFolder())
-					.validateByType(IM.GRAPH_ICD10, cfg.getFolder())
-					.validateByType(IM.GRAPH_VISION, cfg.getFolder())
-					.validateByType(IM.GRAPH_KINGS_APEX, cfg.getFolder())
-					.validateByType(IM.GRAPH_KINGS_WINPATH, cfg.getFolder())
-					.validateByType(IM.GRAPH_BARTS_CERNER, cfg.getFolder())
-					.validateByType(IM.GRAPH_ODS, cfg.getFolder())
-					.validateByType(IM.GRAPH_NHS_TFC, cfg.getFolder())
-					.validateByType(IM.GRAPH_CEG_QUERY,cfg.getFolder())
-					.validateByType(IM.GRAPH_IM1,cfg.getFolder());
+        System.out.println("Validating data files...");
+        TTImportByType importer = new Importer()
+            .validateByType(IM.GRAPH_DISCOVERY, cfg.getFolder())
+            .validateByType(SNOMED.GRAPH_SNOMED, cfg.getFolder())
+            .validateByType(IM.GRAPH_ENCOUNTERS, cfg.getFolder())
+            .validateByType(IM.GRAPH_EMIS, cfg.getFolder())
+            .validateByType(IM.GRAPH_TPP, cfg.getFolder())
+            .validateByType(IM.GRAPH_OPCS4, cfg.getFolder())
+            .validateByType(IM.GRAPH_ICD10, cfg.getFolder())
+            .validateByType(IM.GRAPH_VISION, cfg.getFolder())
+            .validateByType(IM.GRAPH_KINGS_APEX, cfg.getFolder())
+            .validateByType(IM.GRAPH_KINGS_WINPATH, cfg.getFolder())
+            .validateByType(IM.GRAPH_BARTS_CERNER, cfg.getFolder())
+            .validateByType(IM.GRAPH_ODS, cfg.getFolder())
+            .validateByType(IM.GRAPH_NHS_TFC, cfg.getFolder())
+            .validateByType(IM.GRAPH_CEG_QUERY, cfg.getFolder())
+            .validateByType(IM.GRAPH_IM1, cfg.getFolder());
 
-				importer.importByType(IM.GRAPH_DISCOVERY, cfg);
-				importer.importByType(SNOMED.GRAPH_SNOMED, cfg);
-				importer.importByType(IM.GRAPH_ENCOUNTERS, cfg);
-				importer.importByType(IM.GRAPH_EMIS, cfg);
-				importer.importByType(IM.GRAPH_TPP, cfg);
-				importer.importByType(IM.GRAPH_OPCS4, cfg);
-				importer.importByType(IM.GRAPH_ICD10, cfg);
-				importer.importByType(IM.GRAPH_VISION, cfg);
-				importer.importByType(IM.GRAPH_BARTS_CERNER, cfg);
-				importer.importByType(IM.GRAPH_ODS, cfg);
-				importer.importByType(IM.GRAPH_NHS_TFC,cfg);
-				importer.importByType(IM.GRAPH_IM1,cfg);
+        System.out.println("Importing files...");
+        importer.importByType(IM.GRAPH_DISCOVERY, cfg);
+        importer.importByType(SNOMED.GRAPH_SNOMED, cfg);
+        importer.importByType(IM.GRAPH_ENCOUNTERS, cfg);
+        importer.importByType(IM.GRAPH_EMIS, cfg);
+        importer.importByType(IM.GRAPH_TPP, cfg);
+        importer.importByType(IM.GRAPH_OPCS4, cfg);
+        importer.importByType(IM.GRAPH_ICD10, cfg);
+        importer.importByType(IM.GRAPH_VISION, cfg);
+        importer.importByType(IM.GRAPH_BARTS_CERNER, cfg);
+        importer.importByType(IM.GRAPH_ODS, cfg);
+        importer.importByType(IM.GRAPH_NHS_TFC, cfg);
+        importer.importByType(IM.GRAPH_IM1, cfg);
 
-		TCGenerator closureGenerator = TTFilerFactory.getClosureGenerator();
-		closureGenerator.generateClosure(TTBulkFiler.getDataPath(), cfg.isSecure());
+        System.out.println("Generating closure...");
+        TCGenerator closureGenerator = TTFilerFactory.getClosureGenerator();
+        closureGenerator.generateClosure(TTBulkFiler.getDataPath(), cfg.isSecure());
+
+        System.out.println("Preparing bulk filer...");
+        TTBulkFiler.createRepository();
+        startGraph(graphdb);
+
+        System.out.println("Filing into live graph starting with CEG");
+        TTFilerFactory.setBulk(false);
+        TTFilerFactory.setTransactional(true);
+        importer.importByType(IM.GRAPH_KINGS_APEX, cfg);
+        importer.importByType(IM.GRAPH_KINGS_WINPATH, cfg);
+        importer.importByType(IM.GRAPH_CEG_QUERY, cfg);
+        TTImport deltaImporter = new DeltaImporter();
+        deltaImporter.importData(cfg);
+
+        System.out.println("expanding value sets");
+        new SetExpander().expandAllSets();
+        System.out.println("Finished - " + (new Date()));
 
 
-			TTBulkFiler.createRepository();
-			startGraph(graphdb);
-			System.out.println("Filing into live graph starting with CEG");
+        System.out.println("Building text index");
+        new LuceneIndexer().buildIndexes();
+        System.exit(0);
+    }
 
+    public static void validateGraphConfig(String inFolder) {
+        ImportUtils.validateFiles(inFolder, new String[]{
+            ".*config.ttl"});
 
+    }
 
-		  TTFilerFactory.setBulk(false);
-		TTFilerFactory.setTransactional(true);
-			importer.importByType(IM.GRAPH_KINGS_APEX, cfg);
-		importer.importByType(IM.GRAPH_KINGS_WINPATH, cfg);
-	   importer.importByType(IM.GRAPH_CEG_QUERY,cfg);
-			TTImport deltaImporter= new DeltaImporter();
-			deltaImporter.importData(cfg);
+    private static void startGraph(String graphdb) throws IOException, InterruptedException {
 
-		System.out.println("expanding value sets");
-		new SetExpander().expandAllSets();
-		System.out.println("Finished - " + (new Date()));
-
-
-		System.out.println("Building text index");
-		new LuceneIndexer().buildIndexes();
-		System.exit(0);
-	}
-
-	public static void validateGraphConfig(String inFolder){
-		ImportUtils.validateFiles(inFolder, new String[] {
-			".*config.ttl"});
-
-	}
-
-	private static void startGraph(String graphdb) throws IOException, InterruptedException {
-
-		Scanner scanner = new Scanner(System.in);
-		boolean ok = false;
-		while (!ok) {
-			System.out.println("");
-			System.err.println("Please start graph db in the usual manner and enter 'OK' when done : ");
-			String line = scanner.nextLine();
-			if (line.equalsIgnoreCase("ok"))
-				ok = true;
-		}
+        Scanner scanner = new Scanner(System.in);
+        boolean ok = false;
+        while (!ok) {
+            System.out.println("");
+            System.err.println("Please start graph db in the usual manner and enter 'OK' when done : ");
+            String line = scanner.nextLine();
+            if (line.equalsIgnoreCase("ok"))
+                ok = true;
+        }
 
 		 /*
 
@@ -147,6 +151,5 @@ public class Preload {
 		 Thread.sleep(15000);
 
 		  */
-	}
-
+    }
 }
