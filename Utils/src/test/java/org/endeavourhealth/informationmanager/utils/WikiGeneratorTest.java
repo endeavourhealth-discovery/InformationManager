9b775810dcd3ef6d7c8d69b4f83de91cb0890f8e
David Stables <DavidStables@users.noreply.github.com>
2022-08-30 17:18:50 +0100

    Create WikiGeneratorTest.java

---
+++ Utils/src/test/java/org/endeavourhealth/informationmanager/utils/WikiGeneratorTest.java
@@ -0,0 +1,21 @@
+package org.endeavourhealth.informationmanager.utils;
+
+import org.endeavourhealth.imapi.transforms.TTManager;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.zip.DataFormatException;
+
+class WikiGeneratorTest {
+
+	@Test
+	void generateDocsFromCore() throws IOException, DataFormatException {
+		WikiGenerator generator = new WikiGenerator();
+		String documentation = generator.generateDocs();
+		try (FileWriter wr = new FileWriter("c:\\temp\\abnf.txt")) {
+			wr.write(documentation);
+		}
+	}
+}
