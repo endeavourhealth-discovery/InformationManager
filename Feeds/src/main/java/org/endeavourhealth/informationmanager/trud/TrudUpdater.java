94d84c134d471dc2757a4b5eb28059fdc58f0e6c
Richard Collier <gitkraken@tirinoarim.co.uk>
2023-04-12 09:17:07 +0100

    Bump package versions Migrate Javax to Jersey

--- Feeds/src/main/java/org/endeavourhealth/informationmanager/trud/TrudUpdater.java
+++ Feeds/src/main/java/org/endeavourhealth/informationmanager/trud/TrudUpdater.java
@@ -8,10 +8,10 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.Response;
+import jakarta.ws.rs.client.Client;
+import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
+import jakarta.ws.rs.core.Response;
 import java.io.*;
 import java.net.URL;
 import java.net.URLConnection;
