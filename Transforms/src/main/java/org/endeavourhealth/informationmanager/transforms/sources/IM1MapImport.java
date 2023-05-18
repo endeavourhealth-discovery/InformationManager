e14fa31750e7aca717d1dda0453e23542a526348
ZeynepBayazitoglu <zeynep@voror.co.uk>
2023-05-18 10:28:42 +0100

    Added fhir scheme concept

--- Transforms/src/main/java/org/endeavourhealth/informationmanager/transforms/sources/IM1MapImport.java
+++ Transforms/src/main/java/org/endeavourhealth/informationmanager/transforms/sources/IM1MapImport.java
@@ -82,6 +82,8 @@ public class IM1MapImport implements TTImport {
         entities= importMaps.getAllPlusMatches();
         TTManager manager = new TTManager();
         document = manager.createDocument(IM.GRAPH_IM1.getIri());
+        document.addEntity(manager.createGraph(FHIR.GRAPH_FHIR.getIri(), "FHIR code scheme and graph",
+                "The FHIR code scheme and graph, i.e. codes defined in the FHIR specification"));
         newSchemes();
         statsDocument= manager.createDocument(IM.GRAPH_STATS.getIri());
         importv1Codes(inFolder);
