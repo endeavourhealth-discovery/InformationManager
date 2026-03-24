e9033eb6b928c064642f1a108c818b754e0d70ce
David Stables <DavidStables@users.noreply.github.com>
2026-03-24 09:41:52 +0000

    Update CompassIMGenerator.java

--- Utils/src/main/java/compass/CompassIMGenerator.java
+++ Utils/src/main/java/compass/CompassIMGenerator.java
@@ -1,22 +1,30 @@
 package compass;
 
+import org.apache.commons.logging.Log;
 import org.eclipse.rdf4j.query.BindingSet;
 import org.eclipse.rdf4j.query.TupleQuery;
 import org.eclipse.rdf4j.query.TupleQueryResult;
 import org.endeavourhealth.imapi.dataaccess.databases.IMDB;
+import org.endeavourhealth.informationmanager.transforms.sources.SnomedImporter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import java.io.FileWriter;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CompassIMGenerator {
+	private static final Logger LOG = LoggerFactory.getLogger(CompassIMGenerator.class);
 	public void generate(String folder) throws Exception{
 		if (!folder.endsWith("\\")) folder += "\\";
+		LOG.info("Generating Compass tct files");
 		outputTct(folder);
+		LOG.info("Generating Compass concept files");
 		outputConcepts(folder);
 	}
 
 	private void outputConcepts(String folder) throws Exception {
+		Map<String, String> conceptMap = new HashMap<>();
 		try (FileWriter conceptWriter = new FileWriter(folder + "concept_im2.txt")) {
 			try (IMDB conn = IMDB.getConnection()) {
 				String spq = """
@@ -35,9 +43,13 @@ public class CompassIMGenerator {
 						String im1Dbid = bs.getValue("im1Dbid").stringValue();
 						String label = bs.getValue("label").stringValue();
 						String code = bs.getValue("code").stringValue();
-						conceptWriter.write(im1Dbid + "\t" + label + "\t" + code + "\t"+ iri+"\n");
+						conceptMap.put(im1Dbid,code+"\t"+label+"\t"+iri);
+
 					}
 				}
+			}
+			for (Map.Entry<String, String> entry : conceptMap.entrySet()) {
+				conceptWriter.write(entry.getKey()+"\t"+ entry.getValue() + "\n");
 			}
 		}
 	}
