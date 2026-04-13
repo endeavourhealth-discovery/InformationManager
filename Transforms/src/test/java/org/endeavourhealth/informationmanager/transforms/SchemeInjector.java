afde751c80c001cd750736c3442c3fc15ee1c8c4
Richard Collier <richard.collier@voror.co.uk>
2026-04-13 11:05:00 +0100

    refactor(transforms): simplify namespace import in scheme injector
    replaced Namespace import with NAMESPACE constant
    updated entity scheme assignment to use new import
    no functional changes, only import cleanup

--- Transforms/src/test/java/org/endeavourhealth/informationmanager/transforms/SchemeInjector.java
+++ Transforms/src/test/java/org/endeavourhealth/informationmanager/transforms/SchemeInjector.java
@@ -4,8 +4,7 @@ import org.endeavourhealth.imapi.model.tripletree.TTDocument;
 import org.endeavourhealth.imapi.model.tripletree.TTEntity;
 import org.endeavourhealth.imapi.transforms.TTManager;
 import org.endeavourhealth.imapi.vocabulary.IM;
-import org.endeavourhealth.imapi.vocabulary.Namespace;
-import org.endeavourhealth.informationmanager.transforms.sources.ImportUtils;
+import org.endeavourhealth.imapi.vocabulary.NAMESPACE;
 
 import java.io.File;
 import java.nio.file.Path;
@@ -22,7 +21,7 @@ public class SchemeInjector {
       TTDocument document = manager.getDocument();
       for(TTEntity entity : document.getEntities()) {
         if (!entity.has(IM.HAS_SCHEME))
-          entity.set(IM.HAS_SCHEME, Namespace.from(args[1]).asIri());
+          entity.set(IM.HAS_SCHEME, NAMESPACE.from(args[1]).asIri());
       }
 
       manager.setDocument(document);
